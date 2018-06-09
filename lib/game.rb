a3175cf906507129f10845549a519ea529dc2e4a
Dani Lyn <dlyn1110@gmail.com>
2018-06-09 12:05:38 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -31,7 +31,7 @@ WIN_COMBINATIONS = [
   def won?
     WIN_COMBINATIONS.detect do |winner|
       @board.cells[winner[0]] == @board.cells[winner[1]] &&
-      @board.cells[winner[1]] == @board.cells[winner[2]]
+      @board.cells[winner[1]] == @board.cells[winner[2]] &&
       (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
 
    end
